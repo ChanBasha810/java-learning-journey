8d28814a9d4b381806be12f2f26e899e2fb29db4
Chan Basha <imac810a@gmail.com>
2025-11-02 00:06:03 +0530

    Add VariablesExample class with basic variable types

---
+++ Day-2_Variables/VariablesExample.java
@@ -0,0 +1,12 @@
+public class VariablesExample{
+    public static void main(String[] args){
+        int age = 22;
+        String name = "Snafu";
+        double height = 5.8;
+        boolean isStudent = true;
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Height: " + height + " feet");
+        System.out.println("Student: " + isStudent);
+    }
+}

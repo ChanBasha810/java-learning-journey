f0e17abeb2168f26b81e21b0dc2f1257620e585e
Chan Basha <imac810a@gmail.com>
2025-11-02 00:07:48 +0530

    Add DataTypesExample.java with various data types

---
+++ Day-2_Variables/DataTypesExample.java
@@ -0,0 +1,20 @@
+public class DataTypesExample{
+    public static void main(String[] args){
+        byte smallNumber = 10;
+        short shortNumber = 3000;
+        int integerNumber = 50000;
+        long bigNumber = 123456789L;
+        float floatNumber = 3.14f;
+        double doubleNumber = 99.99;
+        char letter = 'A';
+        boolean flag = true;
+        System.out.println("byte value: " + smallNumber);
+        System.out.println("short value: " + shortNumber);
+        System.out.println("int value: " + integerNumber);
+        System.out.println("long value: " + bigNumber);
+        System.out.println("float value: " + floatNumber);
+        System.out.println("double value: " + doubleNumber);
+        System.out.println("char value: " + letter);
+        System.out.println("boolean value: " + flag);
+    }
+}

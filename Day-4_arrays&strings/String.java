8bbca9a3377fdf8b20e0b3a9e0d4992e3bf893d0
Chan Basha <imac810a@gmail.com>
2025-11-04 01:01:17 +0530

    Add String class with greeting example

---
+++ Day-4_arrays&strings/String.java
@@ -0,0 +1,7 @@
+public class String{
+    public static void main(String[] args){
+        String name = "Snafu";
+        String greeting = "Hello, " + name + "!";
+        System.out.println(greeting);
+    }
+}
